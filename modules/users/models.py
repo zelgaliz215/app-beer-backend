771c2f5bf23a5c1a323c94e06c6fe90c25d7d0ab
zelgaliz215 <zelyhobit@gmail.com>
2025-01-02 07:27:19 -0500

    Creacion del modelo de datos de usuarios y rutas para leer y crear usuarios

---
+++ modules/users/models.py
@@ -0,0 +1,17 @@
+# Creacion del modelo SQLalchemy para usuarios
+
+from database import db
+
+class User(db.Model):
+    # Nombre de la tabla
+    __tablename__ = 'users'
+    
+    # Columnas de la tabla
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    
+    # Metodo para imprimir el objeto
+    def __repr__(self):
+        return f'<User {self.username}>'
